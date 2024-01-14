38542ae510327d8707cdab24556e879539806313
Nerya Lifshitz <nerya.lifshitz@opsys-tech.com>
2024-01-14 15:28:09 +0200

    add gitignore and req files

--- NLI_prediction.py
+++ NLI_prediction.py
@@ -1,10 +1,52 @@
+# This Python file uses the following encoding: utf-8
+
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import torch
-
+import wikipedia
+import numpy as np
+import matplotlib.pyplot as plt
 model_name = "MoritzLaurer/multilingual-MiniLMv2-L6-mnli-xnli"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForSequenceClassification.from_pretrained(model_name)
+model = AutoModelForSequenceClassification.from_pretrained(model_name )
 
+def compare_sentenses(s1, s2):
+
+       input = tokenizer(s1, s2, truncation=True, return_tensors="pt")
+       output = model(input["input_ids"])
+
+       prediction = torch.softmax(output["logits"][0], -1).tolist()
+       diff = prediction[2]-prediction[0]
+       label_names = ["entailment", "neutral", "contradiction"]
+       prediction = {name: round(float(pred) * 100, 1) for pred, name in zip(prediction, label_names)}
+       return diff
+
+wikipedia.set_lang("ar")
+ar_list  = wikipedia.page('Al-Ahli Arab Hospital explosion').content.split('\n')
+
+wikipedia.set_lang("he")
+he_list  = wikipedia.page('Al-Ahli Arab Hospital explosion').content.split('\n')
+
+l_1 = len(ar_list)
+
+l_2 = len(he_list)
+print(l_1, l_2) 
+
+heatmap = np.zeros((l_1, l_2))
+for i, s1 in enumerate(ar_list):
+       for j, s2 in enumerate(he_list):
+              heatmap[i, j] = compare_sentenses(s1, s2)
+              print(i, j)
+fig, ax = plt.subplots()
+im = ax.imshow(heatmap)
+
+# Add the color bar
+cbar = ax.figure.colorbar(im, ax = ax)
+cbar.ax.set_ylabel("Color bar", rotation = -90, va = "bottom")
+
+plt.show()    
+#ar_1=ar_1.decode('utf-8','ignore').encode("utf-8")
+t1 = "the explosion was caused by an israeli air strike"
+t2 = "the explosion was caused by a missle shot by hammas"
 en_1 = "On 17 October 2023, an explosion took place in the parking lot of the courtyard of " \
           "al-Ahli Arab Hospital in Gaza City during the 2023 Israel–Hamas war, resulting in " \
           "a large number of fatalities and injuries among displaced Palestinians seeking shelter there."
@@ -39,9 +81,3 @@ es_3 = "a causa de la explosión es discutida. El Ministerio de Salud de Gaza de
        "musulmanes acusaron a Israel de ser el perpetrador. Las autoridades Israelíes han negado la " \
        "acusación con el apoyo de Estados Unidos (su principal aliado)."
 
-input = tokenizer(en_1, es_1, truncation=True, return_tensors="pt")
-output = model(input["input_ids"])
-prediction = torch.softmax(output["logits"][0], -1).tolist()
-label_names = ["entailment", "neutral", "contradiction"]
-prediction = {name: round(float(pred) * 100, 1) for pred, name in zip(prediction, label_names)}
-print(prediction)
